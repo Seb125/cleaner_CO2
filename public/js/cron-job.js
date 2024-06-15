05a4df9e404df7e541bb02394ed0e133cbc40ed5
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-06-15 12:05:38 +0200

    nodemailer on heroku

--- public/js/cron-job.js
+++ public/js/cron-job.js
@@ -1,5 +1,37 @@
 const cron = require('node-cron');
+const nodemailer = require('nodemailer');
 
-cron.schedule('0 * * * *', () => {
-    console.log("llool")
+cron.schedule('* * * * *', () => {
+    // Create a transporter object using SMTP transport
+let transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com', // SMTP hostname
+    secureConnection: false, // TLS requires secureConnection to be false
+    port: 587, // Port for secure SMTP
+    tls: {
+        ciphers: 'SSLv3'
+    },
+    auth: {
+        user: 'schwarz.duscheleit@hotmail.de', // Your email address
+        pass: process.env.NODEMAILER_PWD // Your email password or application-specific password
+    }
+});
+
+// Define email options
+let mailOptions = {
+    from: 'schwarz.duscheleit@hotmail.de', // Sender address
+    to: 'schwarz.duscheleit@hotmail.de', // List of recipients
+    subject: 'Test Email from Node.js', // Subject line
+    text: 'Hello from Node.js!', // Plain text body
+    html: '<p>Hello from <b>Node.js</b>!</p>' // HTML body (optional)
+};
+
+// Send email
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.error('Error sending email:', error);
+    } else {
+        console.log('Email sent:', info.response);
+    }
+});
+
 })
