dfcff2cd46570fb2dc6621f3cf6530d8e1d19efb
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-09-14 10:29:23 +0200

    corrected graph shaded boxes

--- public/js/chart.js
+++ public/js/chart.js
@@ -2,6 +2,69 @@
 
 export function initializeChart(chartData) {
     const ctx = document.getElementById('forecastChart').getContext('2d');
+
+    // calculate shaded boxes
+    let annotation_data = {}
+
+    
+
+    if(chartData.time_frames[0].start < chartData.time_frames[0].end) {
+
+        annotation_data[`shadedRegionGood${0}`] = {
+            type: 'box',
+            xMin: chartData.time_frames[0].start,
+            xMax: chartData.time_frames[0].end,
+            backgroundColor: 'rgba(72, 236, 89, 0.25)'
+        }
+
+    } else {
+
+        annotation_data[`shadedRegionGood${0}`] = {
+            type: 'box',
+            xMin: chartData.time_frames[0].start,
+            xMax: 23,
+            backgroundColor: 'rgba(72, 236, 89, 0.25)'
+        }
+
+        annotation_data[`shadedRegionGood${1}`] = {
+            type: 'box',
+            xMin: 0,
+            xMax: chartData.time_frames[0].end,
+            backgroundColor: 'rgba(72, 236, 89, 0.25)'
+        }
+    }
+
+     
+    if(chartData.time_frames[1].start < chartData.time_frames[1].end) {
+
+        annotation_data[`shadedRegionBad${0}`] = {
+            type: 'box',
+            xMin: chartData.time_frames[1].start,
+            xMax: chartData.time_frames[1].end,
+            backgroundColor: 'rgba(255, 99, 132, 0.25)'
+        }
+
+    } else {
+
+        annotation_data[`shadedRegionBad${0}`] = {
+            type: 'box',
+            xMin: chartData.time_frames[1].start,
+            xMax: 23,
+            backgroundColor: 'rgba(255, 99, 132, 0.25)'
+        }
+
+        annotation_data[`shadedRegionBad${1}`] = {
+            type: 'box',
+            xMin: 0,
+            xMax: chartData.time_frames[1].end,
+            backgroundColor: 'rgba(255, 99, 132, 0.25)'
+        }
+
+
+    }
+    
+
+
     const forecastChart = new Chart(ctx, {
         type: 'line',
         data: {
@@ -23,20 +86,7 @@ export function initializeChart(chartData) {
                     }
                 },
                 annotation: {
-                    annotations: {
-                        shadedRegion1: {
-                            type: 'box',
-                            xMin: chartData.time_frames[0].start, // 1pm (13:00)
-                            xMax: chartData.time_frames[0].end, // 2pm (14:00)
-                            backgroundColor: 'rgba(72, 236, 89, 0.25)'
-                        },
-                        shadedRegion2: {
-                            type: 'box',
-                            xMin: chartData.time_frames[1].start, // 1pm (13:00)
-                            xMax: chartData.time_frames[1].end, // 2pm (14:00)
-                            backgroundColor: 'rgba(255, 99, 132, 0.25)'
-                        }
-                    }
+                    annotations: annotation_data
                 }
             },
             scales: {
